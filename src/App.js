b2a408bb8259df800c9de87144bd3fa8c1dbb31d
haakonmd <haakonmd@stud.ntnu.no>
2021-09-20 21:42:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [width, setWidth] = useState(200);
+  const [height, setHeight] = useState(200);
+  const [blur, setBlur] = useState(1);
+  const [grayScale, setGrayScale] = useState(false);
+  const [ref, setRef] = useState("https://picsum.photos/200/200");
+
+  function getPicture() {
+    if (grayScale) {
+      setRef(
+        "https://picsum.photos/" +
+          width +
+          "/" +
+          height +
+          "?grayscale" +
+          "&blur=" +
+          blur
+      );
+    } else {
+      setRef(
+        "https://picsum.photos/" + width + "/" + height + "/?blur=" + blur
+      );
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+      <div className="parameter">
+        <label className="parameterText">Width</label>
+        <input
+          id="Width"
+          value={width}
+          onChange={(e) => {
+            setWidth(e.target.value);
+          }}
+        />
+      </div>
+      <div className="parameter">
+        <label className="parameterText">Height</label>
+        <input
+          id="Height"
+          value={height}
+          onChange={(e) => {
+            setHeight(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="parameter">
+        <label className="parameterText">Blur</label>
+        <input
+          type="number"
+          id="tentacles"
+          name="tentacles"
+          min="1"
+          max="10"
+          onChange={(e) => {
+            setBlur(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="parameter">
+        <label className="parameterText">Grayscale</label>
+        <input
+          type="checkbox"
+          onClick={() => {
+            setGrayScale(!grayScale);
+          }}
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          getPicture();
+        }}
+        className="button"
+      >
+        <label className="buttonText">Get picture</label>
+      </button>
+      <div className="button">
+        <img src={ref} className="center" />
+      </div>
     </div>
   );
 }
